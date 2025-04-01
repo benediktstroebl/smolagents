39e02dda9941ce50019eb844ad89d8dc823a8317
Benedikt Stroebl <stroebl@princeton.edu>
2025-04-01 19:50:43 +0000

    update

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from examples.open_deep_research.run import create_agent, BROWSER_CONFIG
 from typing import Any
 import os
 
-async def run(sample: dict[str, Any], **kwargs) -> dict[str, Any]:
+def run(input: dict[str, dict], **kwargs) -> dict[str, str]:
+
+    assert 'model_name' in kwargs, 'model_name is required'
+    assert len(input) == 1, 'input must contain only one task'
+    
+    task_id, task = list(input.items())[0]
     
     os.makedirs(f"./{BROWSER_CONFIG['downloads_folder']}", exist_ok=True)
     
     agent = create_agent(model_id=kwargs['model_name'])
         
-    response = await agent.arun(sample["input"][0]["content"])
+    response = asyncio.run(agent.arun(task['Question']))
     
-    return {"output": response}
+    return {task_id: response}
