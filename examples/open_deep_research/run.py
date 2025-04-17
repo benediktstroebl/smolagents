2700fded4c319faea6bbcd3f53cc1675bdf54e89
Patch Creator <patch-creator@example.com>
2025-04-17 20:13:44 +0000

    add

--- examples/open_deep_research/run.py
+++ examples/open_deep_research/run.py
@@ -86,6 +86,7 @@ def create_agent(model_params_input):
     model_params = {
         "custom_role_conversions": custom_role_conversions,
         "max_completion_tokens": 8192,
+        "allowed_openai_params": ['reasoning_effort']
     }
     
     model_params.update(model_params_input)
